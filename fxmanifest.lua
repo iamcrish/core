43ab71368de54acfca7dd4648797f6ca36083663
iamcrish <infokrish@yahoo.com>
2024-08-12 04:41:22 +0100

    MrEverestRP
    Framework - https://mreverest.net

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+version '1.0'
+
+author 'iamkrishuprety @ https://github.com/iamcrish'
+description 'MrEverestRP Framework - https://mreverest.net'
+
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+use_fxv2_oal 'yes'
+
+shared_script 'init.lua'
+
+shared_script 'shared/*.lua'
+client_script 'client/*.lua'
+server_script 'server/*.lua'
